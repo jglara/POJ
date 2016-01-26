ba5d7df2f2c45c5fc17c6a52b2939a5704fe4568
jglara <jglara@gmail.com>
2016-01-26 15:59:35 +0100

    initial

---
+++ 1806/1806.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : 1806.cpp
+// Author      : jglara
+// Version     :
+// Copyright   : 
+// Description : http://poj.org/problem?id=1806
+//============================================================================
+
+#include <iostream>
+
+int main(void) {
+	unsigned int numScenarios(0);
+
+
+	std::cin >> numScenarios;
+	unsigned int scenario(0);
+
+	while (std::cin && scenario < numScenarios) {
+		unsigned int petrolUnits(0);
+
+		std::cin >> petrolUnits;
+
+		std::cout << "Scenario #" << scenario << ":" << std::endl;
+
+		++ scenario;
+	}
+
+
+}
